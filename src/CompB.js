c5c3dbf0d7c14344c899b53f95d99ba0209c7ce7
captainkshitij2 <85981237+captainkshitij2@users.noreply.github.com>
2021-08-30 15:19:43 +0530

    added status function the function for due date has been added.

--- src/CompB.js
+++ src/CompB.js
@@ -1,25 +1,51 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { del } from "./Redux/todoSlice";
-import styles from './styles.css'
+import styles from "./styles.css";
 
 function CompB() {
   const state = useSelector((state) => state.todoSlice);
   const dispatch = useDispatch();
+  ////
+  let dateCheck = "";
+  ///
+  let current_date = new Date();
+  function formatDate(date) {
+    var d = new Date(date),
+      month = "" + (d.getMonth() + 1),
+      day = "" + d.getDate(),
+      year = d.getFullYear();
+
+    if (month.length < 2) month = "0" + month;
+    if (day.length < 2) day = "0" + day;
+    dateCheck = year + month + day;
+
+    return [year, month, day].join("-");
+  }
+
+  let currentDate = formatDate(current_date);
 
   return (
     <>
       {state.value.map((item, index, array) => {
         return (
           <div>
-            <div className='op'>{item.n}</div>
-            <div className='opo'>{item.d}</div>
-            <div className='oppo'>{item.dt}</div>
-            <div></div>
-            <button className='opy'
-              onClick={() => {dispatch(del({ in: index })
-              );}} >
-
+            <div className="op">{item.n}</div>
+            <div className="opo">{item.d}</div>
+            <div className="oppo">{item.dt}</div>
+            <div className="oppo1">
+              {item.dt.replace(/-/g, "") === dateCheck ? (
+                <p>due date is today</p>
+              ) : item.dt.replace(/-/g, "") <= dateCheck ? (
+                <p>Due date is passed</p>
+              ) : null}
+            </div>
+            <button
+              className="opy"
+              onClick={() => {
+                dispatch(del({ in: index }));
+              }}
+            >
               delete
             </button>
           </div>
